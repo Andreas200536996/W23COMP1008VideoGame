12bdd0514e9e46ed279c427540d2c76a1846083a
Andreas200536996 <113151311+Andreas200536996@users.noreply.github.com>
2023-03-30 15:52:40 -0400

    Week 11 - Added the Ship.java class and the ability to control the movement of the ship.

--- src/main/java/com/example/w23comp1008videogame/GameBoardController.java
+++ src/main/java/com/example/w23comp1008videogame/GameBoardController.java
@@ -1,4 +1,70 @@
 package com.example.w23comp1008videogame;
 
+import javafx.animation.AnimationTimer;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.AnchorPane;
+
+import java.util.HashSet;
+
 public class GameBoardController {
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private Button startButton;
+
+    //This variable is used to hold the keys that the game player is pressing
+    private HashSet<KeyCode> activeKeys;
+    @FXML
+    void startGame(ActionEvent event) {
+        startButton.setVisible(false);
+
+        //store the keys pressed in the activeKeys set
+        activeKeys = new HashSet<>();
+        anchorPane.getScene().setOnKeyPressed(keyPressed -> activeKeys.add(keyPressed.getCode())
+        );
+        anchorPane.getScene().setOnKeyReleased(keyReleased -> activeKeys.remove(keyReleased.getCode()));
+
+        Canvas canvas = new Canvas(1000,800);
+
+        //gc could be paintbrush or pencil to better demonstrate its purpose.
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        //get Image objects
+        Image background = new Image(getClass().getResourceAsStream("images/space.png"));
+
+        //Create a sprite that we can draw on our canvas
+        Ship ship = new Ship(300, 500);
+
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                gc.drawImage(background,0,0,canvas.getWidth(),canvas.getHeight());
+                userMovesShip(ship);
+                ship.draw(gc);
+            }
+        };
+        timer.start();
+        anchorPane.getChildren().add(canvas);
+
+    }
+
+    private void userMovesShip(Ship ship) {
+        if (activeKeys.contains(KeyCode.UP))
+            ship.moveUp();
+        if (activeKeys.contains(KeyCode.DOWN))
+            ship.moveDown();
+        if (activeKeys.contains(KeyCode.LEFT))
+            ship.moveLeft();
+        if (activeKeys.contains(KeyCode.RIGHT))
+            ship.moveRight();
+    }
+
 }
---
+++ src/main/java/com/example/w23comp1008videogame/Sprite.java
@@ -0,0 +1,88 @@
+package com.example.w23comp1008videogame;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+public class Sprite {
+    //protected variables can be accessed by any class in the family
+    //for example the ship class is a child of the sprite class
+    protected int posX, posY, imageWidth, imageHeight, speed;
+    private Image image;
+
+    private boolean alive;
+
+    public Sprite(int posX, int posY, int imageWidth, int imageHeight, int speed, Image image) {
+        setPosX(posX);
+        setPosY(posY);
+        setImageWidth(imageWidth);
+        setImageHeight(imageHeight);
+        setSpeed(speed);
+        this.image = image;
+        alive = true;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        if(posX>=0 && posX <=1000)
+            this.posX = posX;
+        else
+            throw new IllegalArgumentException("posX must be in the range of 0-1000");
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    public int getImageWidth() {
+        return imageWidth;
+    }
+
+    public void setImageWidth(int imageWidth) {
+        this.imageWidth = imageWidth;
+    }
+
+    public int getImageHeight() {
+        return imageHeight;
+    }
+
+    public void setImageHeight(int imageHeight) {
+        this.imageHeight = imageHeight;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+
+    public void draw(GraphicsContext gc) {
+        gc.drawImage(image, posX, posY, imageWidth, imageHeight);
+    }
+
+}
